c9752e930a0055030b71b83184ef97a27f75c1c3
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-08-24 23:40:42 +0400

    Add files via upload

--- Result.jsx
+++ Result.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import phoneImage from '../assets/phone.png'
 export default function Result({rate}) {
   return (
-    <div>
-      you selected {rate} out of 5
+    <div className='container'>
+        <div className="phoneimage">
+            <img src={phoneImage} alt="" />
+        </div>
+        <div className="maintext">
+        <div className="resulttext">
+            <p> you selected {rate} out of 5</p>
+        </div>
+        </div>
+        <div className="thanks">
+            Thank you!
+        </div>
+        <div className="appreciate">
+            <p>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+        </div>
+        
     </div>
   )
 }
